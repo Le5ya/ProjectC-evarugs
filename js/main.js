f060e9a6a555966c3c1b47ac41a6b1aa541432b5
Le6ya <liliatolstyh@gmail.comgit config --global user.email liliatolstyh@gmail.com>
2022-03-28 18:59:07 +0300

    test

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
-const modalWindow = document.querySelector(".modal");
-const buttonModals = document.querySelectorAll(".modal__button");
-const modalClose = document.querySelector(".modal__close");
-const body = document.querySelector("body");
+const modalWindow = document.querySelector(".modal")
+const buttonModals = document.querySelectorAll(".modal__button")
+const modalClose = document.querySelector(".modal__close")
+const body = document.querySelector("body")
 
 buttonModals.forEach((item) => {
   item.addEventListener("click", () => {
-    modalWindow.style.display = "flex";
-    body.classList.add("noscroll");
-  });
-});
+    modalWindow.style.display = "flex"
+    body.classList.add("noscroll")
+  })
+})
 modalWindow.addEventListener("click", (e) => {
-  const isModal = e.target.closest(".modal__inner");
+  const isModal = e.target.closest(".modal__inner")
 
   if (!isModal) {
-    modalWindow.style.display = "none";
+    modalWindow.style.display = "none"
   }
-});
+})
 modalClose.addEventListener("click", () => {
-  modalWindow.style.display = "none";
-  body.classList.remove("noscroll");
-});
+  modalWindow.style.display = "none"
+  body.classList.remove("noscroll")
+})
